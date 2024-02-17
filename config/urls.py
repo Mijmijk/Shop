c3c571fcd150721ada8ec28d4a47b964143fcd18
Mijmijk <mijmijkknownall@gmail.com>
2024-02-17 19:26:43 +0300

    bb

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from shop.views import catalog, orders, create_order
+from shop.views import catalog, orders, create_order, product_detail
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -9,7 +9,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 	path('catalog/', catalog, name='catalog'),
 	path('orders/', orders, name= 'orders'),
-	path('create_order/', create_order, name= 'cr_order'),
+	path('create_order/<int:id>/', create_order, name= 'cr_order'),
+	path('product/<int:id>/', product_detail, name= 'product_detail')
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,28 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, redirect
+from .models import Product, Order
+
 
 def catalog(request):
 	products = Product.objects.all()
 	return render(request,"shop/catalog.html", {'all_products': products})
+
+
 def orders(request):
-	return render(request,"shop/orders.html")
-def create_order(request):
-	return render(request,"shop/create_order.html")
+	orders_ = Order.objects.all()
+	return render(request,"shop/orders.html", {'all_orders': orders_})
+
+
+def create_order(request, id):
+	product = Product.objects.get(id=id)
+	if request.method == 'POST':
+		Order.objects.create(
+			product = product,
+			delivery_address = request.POST['delivery_address']
+		)
+		return redirect('orders')
+	return render(request,"shop/create_order.html", {'product': product})
+
+
+def product_detail(request, id):
+	product = Product.objects.get(id=id)
+	return render(request,"shop/product_detail.html", {'product': product})
